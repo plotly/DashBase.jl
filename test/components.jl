74bb96223957afa97de73237828b611a44ca19ec
Alexandr Romanenko <waralex@gmail.com>
2020-06-09 21:54:10 +0300

    additional test

--- test/components.jl
+++ test/components.jl
@@ -37,3 +37,21 @@ using JSON2
     @test res.props.n_clicks == 1
     @test res.props.var"data-id" == 20
 end
+
+@testset "empty wilds" begin
+    test_comp = Component("html_div", "Div", "dash_html_components",
+     [:children, :id, :n_clicks],
+     Symbol[];
+     id = 10, 
+     n_clicks = 1
+     )
+
+    @test get_name(test_comp) == "html_div"
+    @test get_type(test_comp) == "Div"
+    @test get_namespace(test_comp) == "dash_html_components"
+    @test test_comp.id == 10
+    @test test_comp.n_clicks == 1
+
+    @test_throws ErrorException test_comp.var"data-id" = 20
+    @test_throws ErrorException test_comp.aaaaa
+end
