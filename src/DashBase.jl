00e0b56ab19cfe4d7988c16b32d7c09d8f9f0204
Alexandr Romanenko <waralex@gmail.com>
2020-06-11 17:34:33 +0300

    in work

--- src/DashBase.jl
+++ src/DashBase.jl
@@ -3,6 +3,9 @@ import JSON2
 include("components.jl")
 include("registry.jl")
 export Component, push_prop!, get_name, get_type, get_namespace,
-ResourcePkg, Resource, ResourcesRegistry, isdynamic, register_package!, main_registry
+ResourcePkg, Resource, ResourcesRegistry, isdynamic, register_package!, main_registry,
+get_dash_dependencies, get_dash_renderer_pkg, get_componens_pkgs,
+has_relative_path, has_dev_path, has_external_url, get_type,
+get_external_url, get_dev_path, get_relative_path
 
 end # module
