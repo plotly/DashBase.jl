4fbf399b0d51a717040a2df465430ff8e244b6c4
Alexandr Romanenko <waralex@gmail.com>
2020-06-11 16:16:04 +0300

    add exports

--- src/DashBase.jl
+++ src/DashBase.jl
@@ -3,6 +3,6 @@ import JSON2
 include("components.jl")
 include("registry.jl")
 export Component, push_prop!, get_name, get_type, get_namespace,
-ResourcePkg, Resource, ResourcesRegistry, isdynamic
+ResourcePkg, Resource, ResourcesRegistry, isdynamic, register_package!, main_registry
 
 end # module
