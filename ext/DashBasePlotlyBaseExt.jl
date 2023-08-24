fce43fb5a65d5cb0e3e785f5bd088885db83ce30
Étienne Tétreault-Pinard <info@etpinard.xyz>
2023-08-24 12:02:45 -0400

    replace with `PackageExtensionCompat`
    using `Requires` dance as suggested by
    https://pkgdocs.julialang.org/v1.9/creating-packages/#Requires.jl

--- ext/DashBasePlotlyBaseExt.jl
+++ ext/DashBasePlotlyBaseExt.jl
@@ -1,8 +1,10 @@
 module DashBasePlotlyBaseExt
 
-import DashBase
-import PlotlyBase
-import PlotlyBase.JSON
+using DashBase
+
+isdefined(Base, :get_extension) ? (using PlotlyBase) : (using ..PlotlyBase)
+
+const JSON = PlotlyBase.JSON
 
 function DashBase.to_dash(p::PlotlyBase.Plot)
     data = JSON.lower(p)
--- ext/DashBasePlotlyJSExt.jl
+++ ext/DashBasePlotlyJSExt.jl
@@ -1,7 +1,8 @@
 module DashBasePlotlyJSExt
 
-import PlotlyJS
-import DashBase
+using DashBase
+
+isdefined(Base, :get_extension) ? (using PlotlyJS) : (using ..PlotlyJS)
 
 function DashBase.to_dash(p::PlotlyJS.SyncPlot)
     DashBase.to_dash(p.plot)
