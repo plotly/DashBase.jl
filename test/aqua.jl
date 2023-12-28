4f4d122116a061665506b70bcb0a6897e5846790
Étienne Tétreault-Pinard <info@etpinard.xyz>
2023-12-28 10:23:25 -0500

    drop `project_toml_formatting` kwarg
    as it is no longer part of Aqua

--- test/aqua.jl
+++ test/aqua.jl
@@ -2,14 +2,11 @@ using Aqua
 using DashBase
 using Test
 
-# skip `project_toml_formatting` for older version, as
-# the Project.toml formatting generated after `Pkg.update` changed
-project_toml_formatting = VERSION >= v"1.8.0"
 # skip `stale_deps` for newer version, as
 # we no longer load Requires
 stale_deps = VERSION < v"1.9.0"
 
-Aqua.test_all(DashBase; stale_deps, project_toml_formatting)
+Aqua.test_all(DashBase; stale_deps)
 
 if !stale_deps
     @testset "test_stale_deps except for Requires" begin
